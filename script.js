2f4d85cc26039c38a9e21c7ff1b20592def202d7
Ayosunkanmi001 <owolabi.ayosunkanmi@yahoo.com>
2022-07-16 18:38:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function myFunction(message) {
+    document.getElementById("content").innerHTML = "Hello my lovers!";
+    document.getElementById("contact").innerHTML = "I am Ayosunkanmi";
+    document.getElementById("demo").innerHTML = "I love you so much";
+  }
