392fd8199bdc55bf36823fb72d9993a909a26b16
Ayosunkanmi001 <107854631+Ayosunkanmi001@users.noreply.github.com>
2022-07-17 09:47:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,25 @@ function myFunction(message) {
     document.getElementById("content").innerHTML = "Hello my lovers!";
     document.getElementById("contact").innerHTML = "I am Ayosunkanmi";
     document.getElementById("demo").innerHTML = "I love you so much";
-  }
+
+    var text = document.getElementById("content");
+    if(text.style.display === "none"){
+        text.style.display = "block";
+    } else {
+         text.style.display = "none";
+    }
+
+    var text =document.getElementById("contact");
+    if(text.style.display === "none") {
+       text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+
+    var text = document.getElementById("demo");
+    if(text.style.display === "none"){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
