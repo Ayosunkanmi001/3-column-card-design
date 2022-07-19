d76ca5419be16c6dc9135fd6d7d2c3a4af518508
Ayosunkanmi001 <107854631+Ayosunkanmi001@users.noreply.github.com>
2022-07-19 10:42:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-function myFunction(message) {
-    document.getElementById("content").innerHTML = "Hello my lovers!";
-    document.getElementById("contact").innerHTML = "I am Ayosunkanmi";
-    document.getElementById("demo").innerHTML = "I love you so much";
+// // function myFunction(message) {
+//     // document.getElementById("content").innerHTML = "Hello my lovers!";
+//     // document.getElementById("contact").innerHTML = "I am Ayosunkanmi";
+//     document.getElementById("demo").innerHTML = "I love you so much";
 
-    var text = document.getElementById("content");
-    if(text.style.display === "none"){
-        text.style.display = "block";
-    } else {
-         text.style.display = "none";
-    }
+//     var text = document.getElementById("content");
+//     if(text.style.display === "none"){
+//         text.style.display = "block";
+//     } else {
+//          text.style.display = "none";
+//     }
 
-    var text =document.getElementById("contact");
-    if(text.style.display === "none") {
-       text.style.display = "block";
-    } else {
-        text.style.display = "none";
-    }
+//     var text =document.getElementById("contact");
+//     if(text.style.display === "none") {
+//        text.style.display = "block";
+//     } else {
+//         text.style.display = "none";
+//     }
 
-    var text = document.getElementById("demo");
-    if(text.style.display === "none"){
-        text.style.display = "block";
-    } else {
-        text.style.display = "none";
-    }
+//     var text = document.getElementById("demo");
+//     if(text.style.display === "none"){
+//         text.style.display = "block";
+//     } else {
+//         text.style.display = "none";
+//     }
+// }
+
+function myFunction() {
+    alert("Hello my lovers i am Ayosunkanmi i love you so much");
 }
